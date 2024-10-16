085d1ba88bbbb3cc0cbd7c273e90906e0c108de9
Jon <aljonkenfernandez36@gmail.com>
2024-10-16 21:49:44 +0800

    enum changes

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,11 +5,11 @@
     {{-- 'transaction_title', 'description', 'status', 'total_amount', 'transaction_number', --}}
     transaction_title:<input type="text" name="transaction_title" id="transaction_title" placeholder="enter transaction title" required> <br>
     description:<input type="text" name="description" id="description" placeholder="enter description" required> <br>
-    status: <label for="status1">active</label>
-            <input type="radio" name="status" id="status1" value="active" required>
+    status: <label for="status1">successful</label>
+            <input type="radio" name="status" id="status1" value="successful" required>
     
-            <label for="status2">inactive</label>
-            <input type="radio" name="status" id="status2" value="inactive" required> <br>
+            <label for="status2">declined</label>
+            <input type="radio" name="status" id="status2" value="declined" required> <br>
     
     total_amount: <input type="number" name="total_amount" id="total_amount" placeholder="enter total amount" required> <br>
     transaction_number: <input type="number" name="transaction_number" id="transaction_number" placeholder="enter transaction number" required> <br>
