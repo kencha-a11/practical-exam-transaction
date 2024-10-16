ed694f84e1d82c3dd80e1798e36af5ad38f3f5f9
Jon <aljonkenfernandez36@gmail.com>
2024-10-16 21:51:25 +0800

    status edit and create

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -7,11 +7,11 @@
     {{-- 'transaction_title', 'description', 'status', 'total_amount', 'transaction_number', --}}
     transaction_title:<input type="text" name="transaction_title" id="transaction_title" value="{{$account->transaction_title}}" required> <br>
     description:<input type="text" name="description" id="description" value="{{$account->description}}" required> <br>
-    status: <label for="status1">active</label>
-            <input type="radio" name="status" id="status1" value="active" required>
+    status: <label for="status1">successful</label>
+            <input type="radio" name="status" id="status1" value="successful" required>
     
-            <label for="status2">inactive</label>
-            <input type="radio" name="status" id="status2" value="inactive" required> <br>
+            <label for="status2">declined</label>
+            <input type="radio" name="status" id="status2" value="declined" required> <br>
     
     total_amount: <input type="number" name="total_amount" id="total_amount" value="{{$account->total_amount}}" required> <br>
     transaction_number: <input type="number" name="transaction_number" id="transaction_number" value="{{$account->transaction_number}}" required> <br>
